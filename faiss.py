6d21b5fc405c20f99f30c88d95dd0300acd621ab
qgl <760945418@qq.com>
2021-05-26 09:33:48 +0800

    faiss.py

--- faiss.py
+++ faiss.py
@@ -1,10 +1,12 @@
+# Faiss相似向量检索加速
+
 import numpy as np
 import faiss
 import time
 import pandas as pd
 start = time.time()
 xb = np.load('newstitle.npy', allow_pickle=True)
-xq = np.load('xq.npy', allow_pickle=True)
+xq = np.load('xq1.npy', allow_pickle=True)
 print('加载标题时间：',time.time() - start)
 print(xb.shape)
 
@@ -12,11 +14,16 @@ total_news = pd.read_csv('totalnews.csv')
 title = total_news['title']
 d = 768
 
+nlist = 100
+quantizer  = faiss.IndexFlatL2(d)   # build the index
+index = faiss.IndexIVFFlat(quantizer, d, nlist, faiss.METRIC_L2)
 
-index = faiss.IndexFlatL2(d)   # build the index
-print(index.is_trained)
-index.add(xb)                  # add vectors to the index
-print(index.ntotal)
+start1 = time.time()
+assert not index.is_trained
+index.train(xb)
+assert index.is_trained
+print('训练时间：',time.time() - start1)
+index.add(xb) 
 
 k = 4                          # we want to see 4 nearest neighbors
 start2 = time.time()
